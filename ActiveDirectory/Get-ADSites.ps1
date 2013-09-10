d69544cfcc2a695ddb027016098899b32376021e
Nathan Buuck <nbuuck [at] gmail _dot_ com>
2013-09-10 15:28:34 -0400

    Include Get-ADSites.ps1.

---
+++ ActiveDirectory/Get-ADSites.ps1
@@ -0,0 +1,32 @@
+# Return all Sites in the current domain.
+
+[CmdletBinding()]
+Param(
+	[String]$DN = ""
+)
+
+# Load ActiveDirectory PS module if not active.
+if((Get-Module | Where { $_.Name -eq "ActiveDirectory"}).Count -eq 0){
+	Import-Module ActiveDirectory
+}
+
+# Parse the computer's domain if -DN wasn't specified.
+if($DN -eq ""){
+	$domain = ($ENV:USERDNSDOMAIN).Split('.')
+	if($domain -eq "") {
+		Write-Error "The computer is not a member of a Domain and -DN wasn't provided."
+		return
+	}
+	foreach($sub in $domain){
+		Write-Verbose "Parsed: $sub"
+		$DN = $DN + ",DC=$sub"
+	}
+}
+
+#Compose the complete DN of the container.
+if($DN.Substring(0,1) -eq ","){ $DN = $DN.Substring(1,$DN.Length-1) }
+$DN = "CN=Sites,CN=Configuration,$DN"
+Write-Verbose "DN: $DN"
+
+# Retrieve the Site objects.
+Get-ADObject -SearchBase "$DN" -Filter {objectClass -eq "site"} | Sort-Object Name | Select Name
