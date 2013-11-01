4852e8f75df672c64af84420a74a2a6b0726fd7c
Nathan Buuck <nbuuck [at] gmail _dot_ com>
2013-11-01 10:33:21 -0400

    Renaming. Include some Site Link data.

--- ActiveDirectory/Export-ADSiteTopologyKML.ps1
+++ ActiveDirectory/Export-ADSiteTopologyKML.ps1
@@ -1,8 +1,16 @@
+# nbuuck _at_ gmail dot com 
+
+# PURPOSE: Produces a Keyhole Markup Language file representing an Active Directory
+# domain's Site topology.
+# REQUIREMENTS: Requires read access to the Configuration partition of the domain and
+# that the Location attribute of every Site contains a KML-compliant GPS coordinate.
+
+$filePath = "AD-SitesTopology.kml"
+$kmlTemplate = ".\Template-ADSiteTopologyKML.xml"
+$Domain = $($Env:USERDNSDOMAIN)
+
 Clear-Host
 Import-Module ActiveDirectory
-
-# CONSTANTS
-$kmlFile = "AD-SitesTopology.kml"
 
 # FUNCTIONS
 Function Get-SiteCoordinates($siteList){
@@ -23,14 +31,13 @@ Function Get-SiteCoordinates($siteList){
 }
 
 # Get the domain name for use in search bases for AD queries.
-$domains = $Env:USERDNSDOMAIN
-if($domains -eq $null) {
+if($Domain -eq $null) {
 	Write-Error This computer does not belong to a domain.
 	return
 }
-$domains = $domains.Split(".")
-foreach($domain in $domains){
-	$dnSuffix += "DC=$domain,"
+$Domain = $Domain.Split(".")
+foreach($sumdomain in $Domain){
+	$dnSuffix += "DC=$sumdomain,"
 }
 $dnSuffix = $dnSuffix.TrimEnd(',')
 
@@ -53,7 +60,7 @@ $sites = Get-ADObject `
 	Sort-Object Name
 
 # Read our template (minimize KML building in code).
-$kml = Get-Content .\Template-ADSiteTopologyKML.xml
+$kml = Get-Content $kmlTemplate
 $places = ""
 
 # Represent each Site as a single-point Placemark.
@@ -65,13 +72,18 @@ foreach($site in $sites){
 		-Filter { objectClass -eq "server" }
 	
 	$places += "`t`t<Placemark>`n"
+	$desc = ""
 	if($servers -ne $null){
 		$places += "`t`t`t<styleUrl>#site-with-dc</styleUrl>`n"
+		$serverList = ""
+		$servers | ForEach-Object { $serverList += "$($_.Name)," }
+		$serverList = $serverList.TrimEnd(",")
+		$desc += "Domain Controllers: $serverList"
 	}else{
 		$places += "`t`t`t<styleUrl>#site-without-dc</styleUrl>`n"
 	}
 	$places += "`t`t`t<name>$($site.Name)</name>`n"
-	$places += "`t`t`t<description></description>`n"
+	$places += "`t`t`t<description>$desc</description>`n"
 	$places += "`t`t`t<Point>`n"
 	$places += "`t`t`t`t<coordinates>$($site.Location),0.0</coordinates>`n"
 	$places += "`t`t`t</Point>`n"
@@ -81,12 +93,13 @@ foreach($site in $sites){
 # Represent each Site Link as a two point LineString.
 # Not sure how this will behave in domains containing Site Links
 #		with more than two sites. We follow MS' recommendation and express
-#		our topology use Links with two Sites in each Link.
+#		our topology using Links with two Sites in each Link.
 foreach($siteLink in $siteLinks){
 	$places += "`t`t<Placemark>`n"
-	$places += "`t`t<styleUrl>#site-link</styleUrl>`n"
+	$places += "`t`t`t<styleUrl>#site-link</styleUrl>`n"
 	$places += "`t`t`t<name>$($siteLink.Name)</name>`n"
-	$places += "`t`t`t<description></description>`n"
+	$places += "`t`t`t<description>Cost: $($siteLink.Cost)<br/>`n"
+	$places += "`t`t`t`tInterval: $($siteLink.ReplInterval)m</description>`n"
 	$places += "`t`t`t<LineString>`n"
 	$places += "`t`t`t`t<coordinates>"
 	
@@ -101,5 +114,6 @@ foreach($siteLink in $siteLinks){
 # Substitute in the XML of our Placemarks into a placeholder string in the KML.
 $places = $places.TrimEnd("`n")
 $kml = $kml -replace "{PLACEMARKS}", $places
+
 # Use Set-Content so file is ANSI or Maps won't import.
-$kml | Set-Content -Path $kmlFile
+$kml | Set-Content -Path $filePath
