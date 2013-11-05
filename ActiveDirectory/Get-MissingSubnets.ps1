112cd4212a3926e2a106289dc92a36a21796409d
Nathan Buuck <nbuuck [at] gmail _dot_ com>
2013-11-05 08:21:09 -0500

    Add Get-MissingSubnets.ps1

--- ActiveDirectory/Get-MissingSubnets.ps1
+++ ActiveDirectory/Get-MissingSubnets.ps1
@@ -23,15 +23,15 @@ foreach($line in $log){
 if($events.Count -eq 0) { Write-Host "No events to send, exiting... "; return }
 $events = $events | Select -Unique InetAddress, Hostname | Sort-Object InetAddress | FT -AutoSize | Out-String
 
-$body = "The following hosts are contacting the domain in round robin mode because the host resides in a subnet that is not registered in AD Sites & Services:`n"
+$body = "The following hosts reside in a subnet that is not registered in AD Sites & Services:`n"
 $body += $events
 
-$smtpServer = "fe10mail-relay.franklin.fele"
+$smtpServer = "smtp.domain.com"
 $msg = new-object Net.Mail.MailMessage
 $smtp = new-object Net.Mail.SmtpClient($smtpServer)
-$msg.From = "donotreply@fele.com"
-$msg.ReplyTo = "donotreply@fele.com"
-$msg.To.Add("servers@fele.com")
+$msg.From = "donotreply@domain.com"
+$msg.ReplyTo = "donotreply@domain.com"
+$msg.To.Add("you@domain.com")
 $msg.subject = "$($Env:COMPUTERNAME) - AD Sites & Services - Unregistered Subnets"
 $msg.body = $body
 $msg.IsBodyHtml = $false
